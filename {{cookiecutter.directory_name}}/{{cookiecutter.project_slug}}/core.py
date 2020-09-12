78129ba6deb72be08bc57b7a3c1e1903dd28f606
farisachugthai <farischugthai@gmail.com>
2020-09-12 15:29:54 -0400

    Initial commit

---
+++ {{cookiecutter.directory_name}}/{{cookiecutter.project_slug}}/core.py
@@ -0,0 +1,46 @@
+"""Main module.
+
+Console script for {{cookiecutter.project_slug}}.
+"""
+
+import click
+import sys
+import argparse
+{%- if cookiecutter.command_line_interface|lower == 'argparse' % }
+{%- endif % }
+{%- if cookiecutter.command_line_interface|lower == 'click' % }
+{%- endif % }
+
+{% if cookiecutter.command_line_interface|lower == 'click' % }
+
+
+@click.command()
+def main(args=None):
+    """Console script for {{cookiecutter.project_slug}}."""
+    click.echo("Replace this message by putting your code into "
+               "{{cookiecutter.project_slug}}.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+{%- endif % }
+{%- if cookiecutter.command_line_interface|lower == 'argparse' % }
+
+
+def main():
+    """Console script for {{cookiecutter.project_slug}}."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args()
+
+    print("Arguments: " + str(args._))
+    print("Replace this message by putting your code into "
+          "{{cookiecutter.project_slug}}.cli.main")
+    return 0
+
+
+{%- endif % }
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
